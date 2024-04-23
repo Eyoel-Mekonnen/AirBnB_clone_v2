2386965ef060ecfb1e7c4bb7b58fe086f0b42ef2
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-23 17:08:19 +0300

    task7

--- models/base_model.py
+++ models/base_model.py
@@ -52,7 +52,11 @@ class BaseModel:
         for key, value in self.__dict__.items():
             if key == '_sa_instance_state':
                 continue
-            dict_repr[key] = value
+             
+            if isinstance(value, datetime):
+                dict_repr[key] = value.isoformat()
+            else:
+                dict_repr[key] = value
         dict_repr['__class__'] = type(self).__name__
         return dict_repr
 
