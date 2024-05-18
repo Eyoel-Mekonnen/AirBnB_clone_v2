b34405487c04fe73837f7e64dce1d5cfbc5d3a3a
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-05-18 19:15:16 +0300

    task4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    return "HBHB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun():
+    text = text.replace("_", " ")
+    return f'C {escape(text)}'
+
+
+@app.route("/python", defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool():
+    text = text.replace("_", " ")
+    return f'Pyhton {escape(text)}'
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return str(n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
