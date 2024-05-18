edd8be831ebbcfee1d1007ca203102d3cbf647e4
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-05-18 18:02:25 +0300

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python3
+"""Respond with a message using flask"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+
+@app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
