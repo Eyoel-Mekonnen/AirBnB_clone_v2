ad4457e8e0a991746996bcac88b25b05582c488d
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-05-25 21:53:10 +0300

    tasks

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,14 +8,17 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.route("/states_list", strict_slashes=False)
 def states():
     states = storage.all("State")
     return (render_template("7-states_list.html", states=states))
-    
+
+
 @app.teardown_appcontext
 def close_connection(exc):
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
